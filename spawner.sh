0bdd02a0e2fff03d7ac150e57177344213746f3b
irusev <i.rusev@demax.bg>
2018-01-08 16:46:26 +0200

    some formatting

--- spawner.sh
+++ spawner.sh
@@ -10,42 +10,42 @@ removeFlagFileCommand="rm -f "$flagFileDir$2".flag*"
 isFirstWhileIteration=true
 
 function showProcessRestartedSign {
-    echo "" 
-    echo "" 
-    echo "" 
-    echo "##################################################" 
-    echo "#                                                #" 
-    echo "#               process restarted                #" 
-    echo "#                                                #" 
-    echo "##################################################" 
-    echo "" 
-    echo "" 
-    echo "" 
+	echo "" 
+	echo "" 
+	echo "" 
+	echo "##################################################" 
+	echo "#                                                #" 
+	echo "#               process restarted                #" 
+	echo "#                                                #" 
+	echo "##################################################" 
+	echo "" 
+	echo "" 
+	echo "" 
 }
 
 function log {
-    logMessage=$(date)": $1"
-    echo $logMessage >> $reloaderLogFile
+	logMessage=$(date)": $1"
+	echo $logMessage >> $reloaderLogFile
 }
 
 function onScriptFirstStartSugar {
-    log "evaluating '$processToRefreshStartCommand'"
-    echo ""
-    echo ""
-    isFirstWhileIteration=false
+	log "evaluating '$processToRefreshStartCommand'"
+	echo ""
+	echo ""
+	isFirstWhileIteration=false
 }
 
 function onScriptRestartSugar {
-    log "spawning new process due to alteration: $alterationDetails" 
-    showProcessRestartedSign 
+	log "spawning new process due to alteration: $alterationDetails" 
+	showProcessRestartedSign 
 }
 
 function logEventsAndPrettifyConsole {
-    if [ "$isFirstWhileIteration" = true ]; then 
-        onScriptFirstStartSugar
-    else
-        onScriptRestartSugar
-    fi
+	if [ "$isFirstWhileIteration" = true ]; then 
+		onScriptFirstStartSugar
+	else
+		onScriptRestartSugar
+	fi
 }
 
 eval $removeFlagFileCommand
@@ -53,16 +53,16 @@ sleep 1 && echo "" > $flagFileDir$2.flag &
 
 while [ 1 == 1 ]
 do
-    alterationDetails=$(inotifywait -e modify,create  $flagFileDir)
-    if [[ "$alterationDetails" == *"$2.flag"* ]] # if inotifywait output contains substring of arg 2
-    then
+	alterationDetails=$(inotifywait -e modify,create  $flagFileDir)
+	if [[ "$alterationDetails" == *"$2.flag"* ]] # if inotifywait output contains substring of arg 2
+	then
 		log "removing flag file: $removeFlagFileCommand"
-        eval $removeFlagFileCommand
-        logEventsAndPrettifyConsole
-        eval $processToRefreshStartCommand 
-    else 
-        log "%alterationDetails does not match %2" 
-        log "$alterationDetails" 
-        log "$2" 
-    fi
+		eval $removeFlagFileCommand
+		logEventsAndPrettifyConsole
+		eval $processToRefreshStartCommand 
+	else 
+		log "%alterationDetails does not match %2" 
+		log "$alterationDetails" 
+		log "$2" 
+	fi
 done
